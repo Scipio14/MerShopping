26bd0875e681f68abc408a74ebc5319cdf902c06
Federico Macías <fma.abogado@gmail.com>
2022-04-23 10:36:25 -0500

    Finishing the HomeScreen and its components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //Screens
@@ -7,13 +8,16 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 //Components
 import Navbar from "./components/Navbar";
+import Backdrop from "./components/Backdrop";
+import SideDrawer from "./components/SideDrawer";
 
 function App() {
+  const [sidetoggle, setSideToggle] = useState(false);
   return (
     <Router>
-      <Navbar />
-      {/*SideDrawer or Mobile navbar*/}
-      {/*Backdrop*/}
+      <Navbar click={() => setSideToggle(true)} />
+      <SideDrawer show={sidetoggle} click={() => setSideToggle(false)} />
+      <Backdrop show={sidetoggle} click={() => setSideToggle(false)} />
       <main>
         <Routes>
           <Route exact path="/" element={<HomeScreen />} />
