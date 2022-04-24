3f70a013455a34c8044a101025a22187f0ab8e71
Federico Macías <fma.abogado@gmail.com>
2022-04-24 12:46:43 -0500

    Setting up the redux store

---
+++ frontend/src/redux/actions/productActions.js
@@ -0,0 +1,48 @@
+import * as actionTypes from "../constants/productConstants";
+import axios from "axios";
+
+export const getProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: actionTypes.GET_PRODUCTS_REQUEST });
+
+    const { data } = await axios.get("/api/products");
+    dispatch({
+      type: actionTypes.GET_PRODUCTS_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: actionTypes.GET_PRODUCTS_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export const getProductDetail = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch({
+      type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: actionTypes.GET_PRODUCT_DETAILS_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export const removeProductDetails = () => (dispatch) => {
+  dispatch({
+    type: actionTypes.GET_PRODUCT_DETAILS_RESET,
+  });
+};
